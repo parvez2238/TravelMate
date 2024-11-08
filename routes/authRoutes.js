4f2ac40a32d9cb994ffa2af1e67ffdc44511373f
Parvez Rahaman <32400969+parvez2238@users.noreply.github.com>
2024-11-08 23:59:01 +0530

    Add files via upload
    route

---
+++ routes/authRoutes.js
@@ -0,0 +1,11 @@
+// routes/authRoutes.js
+const express = require('express');
+const { signup, login, forgotPassword } = require('../controllers/authController');
+
+const router = express.Router();
+
+router.post('/signup', signup);
+router.post('/login', login);
+router.post('/forgot-password', forgotPassword);
+
+module.exports = router;
