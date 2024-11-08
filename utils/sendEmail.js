2739d0866d504fefdb7de758a3f4763c6cca5573
Parvez Rahaman <32400969+parvez2238@users.noreply.github.com>
2024-11-08 23:58:45 +0530

    Add files via upload
    utils folder

---
+++ utils/sendEmail.js
@@ -0,0 +1,24 @@
+// utils/sendEmail.js
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
+const sendEmail = async (options) => {
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.EMAIL,
+    to: options.to,
+    subject: options.subject,
+    text: options.text,
+  };
+
+  await transporter.sendMail(mailOptions);
+};
+
+module.exports = sendEmail;
